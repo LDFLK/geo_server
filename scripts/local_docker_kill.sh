23cd5befdcbff688af09d57722cb20d9af0b2410
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-05 16:40:38 +0530

    Fixed docker kill bug

--- scripts/local_docker_kill.sh
+++ scripts/local_docker_kill.sh
@@ -1,2 +1,3 @@
 docker kill local_geo_server
+docker rm /local_geo_server
 docker ps
--- scripts/local_docker_run.sh
+++ scripts/local_docker_run.sh
@@ -1,3 +1,4 @@
 docker kill local_geo_server
+docker rm /local_geo_server
 docker run -p 82:82 -d --name local_geo_server nuuuwan/geo_server
 docker ps
