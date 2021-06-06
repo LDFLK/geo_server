a9c8cbadbf9bc21f26959aa721e41436016f9b34
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-06 09:43:20 +0530

    Updated test_geo_server.py

--- test_geo_server.py
+++ test_geo_server.py
@@ -1,40 +1,14 @@
 """Test."""
-import sys
-import os
 import unittest
-import time
-
-from utils.flask import FlaskClient
-
-N_SAMPLES = 5
-
-SERVER_NAME, HOST, PORT = None, '127.0.0.1', 5000
-# SERVER_NAME, HOST, PORT = 'geo', '0.0.0.0', 8080
-# SERVER_NAME, HOST, PORT = 'geo', '18.209.43.63', 80
+import geo_server
 
 
 class TestGeoServer(unittest.TestCase):
     """Tests."""
 
-    def setUp(self):
-        """Init test."""
-        if not SERVER_NAME:
-            os.system('./test_local_flask_stop.sh')
-            print('Starting flask...')
-            os.system('./test_local_flask_start.sh &')
-            time.sleep(2)
-        self.__client = FlaskClient(SERVER_NAME, HOST, PORT)
-
-    def tearDown(self):
-        """Teardown test."""
-        if not SERVER_NAME:
-            print('Stopping flask...')
-            os.system('./test_local_flask_stop.sh')
-
     def test_latlng_to_region(self):
         """Test."""
-        latlng_str_1 = '6.9157,79.8636'
-        regions = self.__client.run('latlng_to_region', [latlng_str_1])
+        regions = geo_server.latlng_to_region('6.9157,79.8636')
         self.assertEqual(
             regions,
             {
@@ -45,8 +19,7 @@ class TestGeoServer(unittest.TestCase):
             },
         )
 
-        latlng_str_2 = '20,20'
-        regions = self.__client.run('latlng_to_region', [latlng_str_2])
+        regions = geo_server.latlng_to_region('20,20')
         self.assertEqual(
             regions,
             {},
@@ -54,12 +27,12 @@ class TestGeoServer(unittest.TestCase):
 
     def test_region_geo(self):
         """Test."""
-        geo = self.__client.run('region_geo', ['LK-11'])
+        geo = geo_server.region_geo('LK-11')
         self.assertEqual(
             geo['type'],
             'MultiPolygon',
         )
-        geo = self.__client.run('region_geo', ['LK-15'])
+        geo = geo_server.region_geo('LK-15')
         self.assertEqual(
             geo,
             {},
@@ -67,8 +40,7 @@ class TestGeoServer(unittest.TestCase):
 
     def test_altitude(self):
         """Test."""
-        latlng_str_1 = '6.9157,79.8636'
-        alt = self.__client.run('altitude', [latlng_str_1])
+        alt = geo_server.altitude('6.9157,79.8636')
         self.assertEqual(
             alt,
             {'altitude': 12},
